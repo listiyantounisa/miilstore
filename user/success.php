fee520697f68b6fe3626df087b33e82a7a96d766
Eirene Kristina Wiyanto <eirenekw@gmail.com>
2019-07-02 15:58:13 +0700

    miistore

---
+++ user/success.php
@@ -0,0 +1,6 @@
+<div class="container">
+	<div class="text-center">
+		<h1>Sukses</h1>
+		<p>Terima kasih telah mendaftar kepada kami. Anda telah menjadi anggota MiiStore. Silahkan klik "Masuk".</p>
+	</div>
+</div>
